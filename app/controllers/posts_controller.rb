b3941ef083bb908bb041ea11087e4093a8f5ec12
Aynur Galimov <galimovaynur@protonmail.com>
2021-07-01 21:20:57 +0500

    add posts to user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[ new create ]
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       redirect_to root_path
